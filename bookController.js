8f527f1f7783c62b93bb3c2d7ef37762485ff8a8
AhmedMuktadirUdoy <100287308+AhmedMuktadir14218@users.noreply.github.com>
2024-05-30 22:12:04 +0600

    Add files via upload

---
+++ bookController.js
@@ -0,0 +1,47 @@
+const Book = require('../models/book');
+
+exports.addBook = async (req, res) => {
+  try {
+    const { title, author, publishedDate, pages, genre } = req.body;
+    const newBook = new Book({ title, author, publishedDate, pages, genre });
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.getBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.updateBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedBook = await Book.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+    if (!updatedBook) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedBook = await Book.findByIdAndDelete(id);
+    if (!deletedBook) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+    res.status(200).json({ message: 'Book deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
